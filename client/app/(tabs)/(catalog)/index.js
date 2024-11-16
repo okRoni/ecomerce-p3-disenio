343c30c4e5146b242858389daf34d18f41fa80e8
Oscar Roni <oscar.gt.140.ro@gmail.com>
2024-11-16 15:35:39 -0600

    item de catalogo

--- client/app/(tabs)/(catalog)/index.js
+++ client/app/(tabs)/(catalog)/index.js
@@ -4,13 +4,16 @@ import CatalogItem from '../../../components/CatalogItem';
 
 export default function CatalogScreen() {
   const car = {
-    name: 'Carro de ejemplo',
+    model: 'Carro de ejemplo',
+    year: 2024,
+    brand: 'Marca Ejemp',
+    price: 9999999.99,
     image: null
   };
   return (
     <View className="flex-1">
       <LinkButton href="/car-details">Detalles de auto</LinkButton>
-      <CatalogItem car={car} />
+      <CatalogItem model={car.model} year={car.year} brand={car.brand} image={car.image} price={car.price} />
     </View>
   );
 }
