204e4f6e917dc38c4c7e748c115483854b115b27
Oscar Roni <oscar.gt.140.ro@gmail.com>
2024-11-18 03:36:12 -0600

    Fix login menajes y IP dinamica

--- client/app/(tabs)/(user)/login.js
+++ client/app/(tabs)/(user)/login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
 import { useRouter } from 'expo-router';  // Import the useRouter hook
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
 
 export default function LoginScreen() {
   const [correo_electronico, setCorreoElectronico] = useState('');
@@ -11,7 +12,7 @@ export default function LoginScreen() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://192.168.0.14:3000/api/login', {
+      const response = await fetch(`http://${process.env.EXPO_PUBLIC_API_IP}:3000/api/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ correo_electronico, password }),
@@ -25,16 +26,31 @@ export default function LoginScreen() {
         
         // Show success alert
         Alert.alert('Inicio de sesión exitoso', 'Bienvenid@ de vuelta, ha iniciado sesión');
+        Toast.show({
+          type: 'success',
+          text1: 'Inicio de sesión exitoso',
+          text2: 'Bienvenid@ de vuelta, ha iniciado sesión',
+        });
         
         // Navigate to the index screen
         router.push('../index.js');  // Use router.push() to navigate to the index screen
       } else {
         console.error('Error iniciando sesión', data.message);
         Alert.alert('Error de inicio de sesión', data.message); // Display an alert if login fails
+        Toast.show({
+          type: 'error',
+          text1: 'Error de inicio de sesión',
+          text2: data.message,
+        });
       }
     } catch (error) {
       console.error('Error iniciando sesión', error);
       Alert.alert('Error', 'Ha ocurrido un error al iniciar sesión'); // Alert on error
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Ha ocurrido un error al iniciar sesión',
+      });
     }
   };
 
@@ -54,6 +70,7 @@ export default function LoginScreen() {
         secureTextEntry
       />
       <Button title="Login" onPress={handleLogin} color="#007BFF" />
+      <Toast/>
     </View>
   );
 }
